07304d9362b3f47f1561d264fe86146129d51fee
Babin Bohora <bohorababin1@gmail.com>
2022-10-08 13:02:25 +0900

    Feature board feature

--- components/kanban/MainPlayground.js
+++ components/kanban/MainPlayground.js
@@ -1,27 +1,25 @@
-import React, { useCallback } from 'react';
-import styled, { css } from 'styled-components';
-import { darkTheme } from '../../styles/color';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Button } from '../common/Button';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { newColumnModalState } from '../../atoms/newColumnModalAtom';
-import { columnsState } from '../../atoms/columnsAtom';
-import { newTaskModalState } from '../../atoms/newTaskModalAtom';
-import { selectedState } from '../../atoms/selectedAtom';
-import { selectedTask } from '../../atoms/selectedTask';
-import { doneCount } from '../../services/doneCount';
-import { signOut } from "next-auth/react"
+import React, { useCallback } from "react";
+import styled, { css } from "styled-components";
+import { darkTheme } from "../../styles/color";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Button } from "../common/Button";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { newColumnModalState } from "../../atoms/newColumnModalAtom";
+import { columnsState } from "../../atoms/columnsAtom";
+import { newTaskModalState } from "../../atoms/newTaskModalAtom";
+import { selectedState } from "../../atoms/selectedAtom";
+import { selectedTask } from "../../atoms/selectedTask";
+import { doneCount } from "../../services/doneCount";
+import { signOut } from "next-auth/react";
 
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import { deleteDoc, doc } from 'firebase/firestore';
-import { db } from '../../core/firebase';
-import { projectsState } from '../../atoms/projectsAtoms';
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { projectsState } from "../../atoms/projectsAtoms";
 
 const MainPlayGroundContainer = styled.div`
   width: 100%;
   overflow: hidden;
-`
+`;
 
 const Heading = styled.div`
   background: ${darkTheme.sideBg};
@@ -31,13 +29,13 @@ const Heading = styled.div`
   padding: 0 1.2rem;
   align-items: center;
   justify-content: space-between;
-`
+`;
 
 const Options = styled.div`
   svg {
     vertical-align: middle;
   }
-`
+`;
 
 const PlayGround = styled.div`
   background: ${darkTheme.bodyBg};
@@ -45,7 +43,7 @@ const PlayGround = styled.div`
   height: calc(100vh - 5rem);
   // overflow: hidden;
   padding: 1rem;
-  font-size: .8rem;
+  font-size: 0.8rem;
   display: flex;
   gap: 1rem;
   overflow: scroll;
@@ -57,39 +55,40 @@ const PlayGround = styled.div`
   &::-webkit-scrollbar {
     display: none; /* Chrome, Safari, Opera */
   }
-`
+`;
 
 const Column = styled.div`
-  width: 260px; 
+  width: 260px;
   max-width: 70vw;
   height: 100%;
   grow: 1;
-  
-  ${props => props.addNew && css`
-  background-color: ${darkTheme.sideBg};
-  opacity: .4;
-  margin-top: 2rem;
-  display: flex;
-  align-items: center;
-  `
-}
-`
+
+  ${(props) =>
+    props.addNew &&
+    css`
+      background-color: ${darkTheme.sideBg};
+      opacity: 0.4;
+      margin-top: 2rem;
+      display: flex;
+      align-items: center;
+    `}
+`;
 
 const Dot = styled.h4`
   text-transform: uppercase;
-  letter-spacing: .5px;
+  letter-spacing: 0.5px;
   margin: 0 0 0 1rem;
   // margin-left: 1rem;
   // position: relative;
   position: sticky;
   top: 0;
-  padding-left: .8rem;
+  padding-left: 0.8rem;
   color: ${darkTheme.secondaryText};
-  
+
   background: ${darkTheme.bodyBg};
-  opacity: .5;
+  opacity: 0.5;
   backdrop-filter: blur(1px);
-  
+
   &:before {
     content: "";
     position: absolute;
@@ -97,14 +96,14 @@ const Dot = styled.h4`
     bottom: 50%;
     transform: translateY(50%);
     border-radius: 50%;
-    width: .8rem;
-    height: .8rem;
+    width: 0.8rem;
+    height: 0.8rem;
     background: white;
   }
-  `
+`;
 
 const Card = styled.div`
-  width: 260px; 
+  width: 260px;
   line-height: 125%;
   font-weight: 500;
   background: ${darkTheme.sideBg};
@@ -112,21 +111,23 @@ const Card = styled.div`
   padding: 1.2rem 1rem;
   color: ${darkTheme.primaryText};
 
-  border-radius: .8rem;
+  border-radius: 0.8rem;
 
   min-height: 5rem;
   height: auto;
 
-  ${props => props.addNew && css`
-    color: ${darkTheme.secondaryText};
-    font-size: 1.5rem;
-    text-align: center;
-  `}
-`
-
+  ${(props) =>
+    props.addNew &&
+    css`
+      color: ${darkTheme.secondaryText};
+      font-size: 1.5rem;
+      text-align: center;
+    `}
+`;
 
 const MainPlayground = () => {
-  const [openNewColumn, setOpenNewColumnn] = useRecoilState(newColumnModalState);
+  const [openNewColumn, setOpenNewColumnn] =
+    useRecoilState(newColumnModalState);
   const [openNewTask, setOpenNewTask] = useRecoilState(newTaskModalState);
   const [projects, setProjects] = useRecoilState(projectsState);
   const columns = useRecoilValue(columnsState);
@@ -148,42 +149,58 @@ const MainPlayground = () => {
   // Delete Board
   const handleDeleteBoard = useCallback(() => {
     if (!selectedBoard?.id) return;
-    const docRef = doc(db, 'projects', selectedBoard.id)
-    deleteDoc(docRef)
-      .then(() => {
-        let newProjects = [];
+    const references = [`projects/${selectedBoard.id}`];
+    
+    // Looping to get all ids 
+    columns.forEach((column) => {
+      column?.items.forEach((task) => {
+        references.push(`tasks/${task.id}`)
+      });
+      references.push(`projects/${column.boardId}/columns/${column.id}`);
+    });
 
-        // filter oldProjects
-        setProjects((oldProjects) => {
-          newProjects = oldProjects.filter((oldProject) => selectedBoard.id !== oldProject.id);
+    // Making a call
+    fetch("/api/boards/delete", {
+      method: "POST",
+      body: JSON.stringify(references)
+    }).then((res) => res.json())
+    .then((data) => {
+      console.log(' Data ', data);
+      let newProjects = [];
+      // Creating Projects
+      setProjects((oldProjects) => {
+        newProjects = oldProjects.filter(
+          (oldProject) => selectedBoard.id !== oldProject.id
+          );
           return newProjects;
         });
         
         // Set 1st project as selected project.
-        if (newProjects?.length > 0) setSelectedBoard(newProjects?.[0]);
-      })
-      .catch(error => {
-        console.error("Error while Deleting Board :---: ", error);
-      })
-      handleClose();
-  }, [selectedBoard.id, setProjects, setSelectedBoard])
+      if (newProjects?.length > 0) setSelectedBoard(newProjects?.[0]);
+    })
+    .catch((error) => console.log("DELETE FAILED :---: ", error));
+
+    handleClose();
+  }, [columns, selectedBoard, setProjects, setSelectedBoard]);
 
   return (
     <MainPlayGroundContainer>
       <Heading>
-        <h2 style={{margin: "0"}}>{selectedBoard?.name ?? "selected a board"}</h2>
+        <h2 style={{ margin: "0" }}>
+          {selectedBoard?.name ?? "selected a board"}
+        </h2>
         <Options>
-          {columns.length > 0 && 
+          {columns.length > 0 && (
             <Button onClick={() => setOpenNewTask(true)}>+Add New Task</Button>
-          }
-          <MoreVertIcon onClick={handleClick}/>
+          )}
+          <MoreVertIcon onClick={handleClick} />
           <Menu
             id="basic-menu"
             anchorEl={anchorEl}
             open={open}
             onClose={handleClose}
             MenuListProps={{
-              'aria-labelledby': 'basic-button',
+              "aria-labelledby": "basic-button",
             }}
           >
             <MenuItem onClick={handleDeleteBoard}>Delete Board</MenuItem>
@@ -194,32 +211,28 @@ const MainPlayground = () => {
       </Heading>
       <PlayGround>
         {/* Previous Columns */}
-        {
-          columns.map(column => (
-          <Column
-            key={column.id}          >
-            <Dot>{column.name} ( {column.items?.length ?? 0} )</Dot>
-            {column.items.map((card) => 
-            (<Card key={card.id} 
-              onClick={() => setShowTask(card)}
-            >
-              {card.name}
-              <br />
-              <p style={{color: darkTheme.secondaryText}}>
-                {!!card.subTasks.length && `${doneCount(card.subTasks)} of ${card.subTasks.length} sub-tasks`}
-              </p>
-            </Card>))}
+        {columns.map((column) => (
+          <Column key={column.id}>
+            <Dot>
+              {column.name} ( {column.items?.length ?? 0} )
+            </Dot>
+            {column.items.map((card) => (
+              <Card key={card.id} onClick={() => setShowTask(card)}>
+                {card.name}
+                <br />
+                <p style={{ color: darkTheme.secondaryText }}>
+                  {!!card.subTasks.length &&
+                    `${doneCount(card.subTasks)} of ${
+                      card.subTasks.length
+                    } sub-tasks`}
+                </p>
+              </Card>
+            ))}
           </Column>
-          ))
-        }
+        ))}
         {/* Add New Column */}
-        <Column
-          addNew
-          onClick={() => setOpenNewColumnn(true)}
-        >
-          <Card addNew>
-            +New Column
-          </Card>
+        <Column addNew onClick={() => setOpenNewColumnn(true)}>
+          <Card addNew>+New Column</Card>
         </Column>
       </PlayGround>
     </MainPlayGroundContainer>
