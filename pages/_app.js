518c2d58cd6cc0971f1600753978ebb6a3d2ae83
Babin Bohora <bohorababin1@gmail.com>
2022-10-10 00:34:06 +0900

    Loading stage done

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.css'
-import { RecoilRoot } from "recoil"
+import { RecoilRoot } from "recoil";
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps: { session, ...pageProps} }) {
   return (
