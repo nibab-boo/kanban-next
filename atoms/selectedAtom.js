5294f1ca8e00748f79c93b81e97066ab7ae316cc
Babin Bohora <bohorababin1@gmail.com>
2022-10-10 00:41:28 +0900

    Using recoi-persist

--- atoms/selectedAtom.js
+++ atoms/selectedAtom.js
@@ -1,6 +1,10 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const selectedState = atom({
   key: "selectedState",
   default: null,
+  effects_UNSTABLE: [persistAtom],
 })
