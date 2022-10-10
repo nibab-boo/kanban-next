543cff3b9f75a25c34f1fbe1de9e1532125b4135
Babin Bohora <bohorababin1@gmail.com>
2022-10-10 22:33:34 +0900

    Apis and Firestore type

--- pages/api/delete/index.ts
+++ pages/api/delete/index.ts
@@ -1,9 +1,10 @@
 import { doc, writeBatch } from "firebase/firestore";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { unstable_getServerSession } from "next-auth";
 import { db } from "../../../core/firebase";
 import { authOptions } from "../auth/[...nextauth]";
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Authorization check
   const session = await unstable_getServerSession(req, res, authOptions);
   if (!session) return res.status(401).json({ error: "Unauthorized User" });
@@ -14,7 +15,7 @@ export default async function handler(req, res) {
 
   // Deleting in Batch
   const batch = writeBatch(db);
-  const references = JSON.parse(req.body);
+  const references: string[] = JSON.parse(req.body);
   references?.forEach((ref) => {
     const docRef = doc(db, ...ref.split('/'));
     batch.delete(docRef);
